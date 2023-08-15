fb2b7d3db995136192fed536c6504a221b49f0d5
schultz77 <138313957+schultz77@users.noreply.github.com>
2023-08-15 15:49:29 +0200

    Update camera_tracking_faces_classy_slots_v4.py refix fps calculation

--- CAM-Tracking/camera_tracking_faces_classy_slots_v4.py
+++ CAM-Tracking/camera_tracking_faces_classy_slots_v4.py
@@ -191,8 +191,8 @@ myTrack = Tracking(tiltAngle=-30, disp={'width': dispW, 'height':dispH}, filteri
 faceCascade = cv2.CascadeClassifier(WD + '/haar/haarcascade_frontalface_default.xml')
 # eyesCascade = cv2.CascadeClassifier('/home/gecko/Dokumente/camera_1_0/haar/haarcascade_eye.xml')
 
-tStart=time.time()
 while True:
+    tStart=time.time()
     frame= myCam.getFrame()
     if len(frame):
         frame=cv2.flip(frame,-1) # flipping frame by 180 degrees
